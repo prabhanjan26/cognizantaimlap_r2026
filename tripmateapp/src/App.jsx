8186730b8bca835a0cff10030f3fa169992efefb
Parameswari Ettiappan <parameswaribala@gmail.com>
2026-06-22 16:01:13 +0530

    updated

--- tripmateapp/src/App.jsx
+++ tripmateapp/src/App.jsx
@@ -1,14 +1,20 @@
 import Logo from './Logo/Logo';
 import Timer from './Timer/Timer';
+import Flight from './Flights/Flight';
 import './App.css';
 function App() {
 
 
   return (
+    <section>
     <section className="app">
       <Logo />
       <h1>Travel App</h1>
       <Timer className="timer" />
+    </section>
+    <section>
+      <Flight></Flight>
+    </section>
     </section>
   )
 }
