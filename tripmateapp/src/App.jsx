26b30e47d281cdf71bdd257b23626dffa53a9267
Parameswari Ettiappan <parameswaribala@gmail.com>
2026-06-22 15:00:18 +0530

    updated

--- tripmateapp/src/App.jsx
+++ tripmateapp/src/App.jsx
@@ -1,12 +1,13 @@
 import Logo from './Logo/Logo';
+import './App.css';
 function App() {
 
 
   return (
-    <>
+    <section className="app">
       <Logo />
       <h1>Travel App</h1>
-    </>
+    </section>
   )
 }
 
